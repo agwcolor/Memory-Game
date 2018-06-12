757d933dee6e1c91c91a398641ad847a3121f7ca
Abbie Weisenbloom <abbiew@froggie.com>
2018-06-12 11:39:02 -0700

    Adding to deck Array

--- js/app.js
+++ js/app.js
@@ -5,12 +5,16 @@
  const pair2 = "fa-anchor";
  const pair3 = "fa-leaf";
  const pair4 = "fa-bomb";
-  const pair5 = "fa-paper-plane-o";
+ const pair5 = "fa-paper-plane-o";
  const pair6 = "fa-bolt";
  const pair7 = "fa-cube";
  const pair8 = "fa-bicyle";
-const deck = [pair1, pair2, pair3, pair4, pair5, pair6, pair7, pair8]
-console.log(deck);
+const deck = [pair1, pair1, pair2, pair2, pair3, pair3, pair4, pair4, pair5, pair5, pair6, pair6, pair7,pair7, pair8, pair8]
+console.log(
+"Hey this is the deck at the start!" + deck);
+
+ var turnedCards = [];
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -33,42 +37,67 @@ function shuffle(array) {
  *   - shuffle the list of cards using the provided "shuffle" method below
  
  /*initialize page*/
+function startOver() {
 const cards = document.querySelectorAll('.card');
-const image = document.querySelectorAll('.fa');
-console.log(image);
 console.log(cards);
 for (i=0; i<cards.length; i++) {
 //for (const card of cards) {
 	console.log(cards[i]);
 cards[i].classList.remove("match", "open", "show");
 //image[i].classList.remove("fa-\*");
-cards[i].innerHTML = '';
-
+//cards[i].innerHTML = '';
 //cards[i].removeChild(image[i]);
 }
-
-console.log(image);
-console.log(cards);
+console.log("Classes match/open/show should be removed" + cards); 
+return cards;
+}
 /*   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
-const shuffledDecks = shuffle(deck);
-//for (const shuffledDeck of shuffledDecks) 
-for (i=0; i<cards.length; i++) {
+var cards=startOver(); /*Start Game Over on refresh*/
+const shuffledDecks = shuffle(deck); /*return shuffled deck from original deck arrary*/
+//?whynotwork?for (const shuffledDeck of shuffledDecks) 
+/* add card images to cards */
+for (i=0; i<cards.length; i++) { 
 	const cardImage=`<i class="fa ${shuffledDecks[i]}"></i>`;
 	console.log(cardImage);
 	console.log(cards);
 	cards[i].innerHTML=cardImage;
+	cards[i].addEventListener('click', openCardList) 
 }
 
+function openCardList { 
+	     turnedCards.push(this);
+	     console.log(this + "is what this is referring to");
+	     if (turnedCards.length === 2) {
+			if (turnedCards[0] == turnedCards[1]){
+				console.log(it's a match!)
+			}
+		 }
+         //displaySymbol();
+}
+/*
 
+var clicks = 0; // counter 
+var a = document.getElementById('avalue'); // element
+a.onclick = function(b) { // onclick not onClick
+   console.log(++clicks); // increment it
+}
 
+addToOpenCardList(); //and check for match
+         lockInOpenPosition();
+         incrementCounter(); //and display it
+         allMatchDisplay();
+
+document.querySelectorAll */
 
 
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
+
+
+
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
