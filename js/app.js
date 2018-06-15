567dcc506674f4f891e75a3312c1b3921e36d5da
Abbie Weisenbloom <abbiew@froggie.com>
2018-06-14 18:22:56 -0700

    Turn Cards | Check if pair

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
-/*
- * Create a list that holds all of your cards
- */
- const pair1 = "fa-diamond";
- const pair2 = "fa-anchor";
- const pair3 = "fa-leaf";
- const pair4 = "fa-bomb";
- const pair5 = "fa-paper-plane-o";
- const pair6 = "fa-bolt";
- const pair7 = "fa-cube";
- const pair8 = "fa-bicyle";
-const deck = [pair1, pair1, pair2, pair2, pair3, pair3, pair4, pair4, pair5, pair5, pair6, pair6, pair7,pair7, pair8, pair8]
-console.log(
-"Hey this is the deck at the start!" + deck);
+const fontMix1 = ["diamond", "anchor", "leaf", "bomb", "paper-plane-o", "bolt", "cube", "bicycle"]
+let turnedCards = [];
+let openedCardList = [];
+let totalOpenedCards = "";
 
- var turnedCards = [];
-
+// Make Deck from chosen font mix
+function makeDeck(array) {
+    let deck = [];
+    for (index in fontMix1) {
+        deck.push(fontMix1[index]);
+        deck.push(fontMix1[index]);
+        console.log(`This is the length of the fonts Array! --> ${deck.length}`);
+    }
+    console.log(`Hey this is the deck at the start! --> ${deck}`);
+    return deck;
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, 
-    temporaryValue, 
-    randomIndex;
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -33,73 +32,98 @@ function shuffle(array) {
     return array;
 }
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- 
- /*initialize page*/
+ * 'Turn over' the cards on the page by removing match/open/show classes
+ /* Return array of .cards classes*/
 function startOver() {
-const cards = document.querySelectorAll('.card');
-console.log(cards);
-for (i=0; i<cards.length; i++) {
-//for (const card of cards) {
-	console.log(cards[i]);
-cards[i].classList.remove("match", "open", "show");
-//image[i].classList.remove("fa-\*");
-//cards[i].innerHTML = '';
-//cards[i].removeChild(image[i]);
+    const cards = document.querySelectorAll('.card');
+    console.log(cards);
+    for (i = 0; i < cards.length; i++) {
+        //for (const card of cards) {
+        cards[i].classList.remove("match", "open", "show");
+        console.log(cards[i]);
+    return cards;
 }
-console.log("Classes match/open/show should be removed" + cards); 
-return cards;
+
+function displaySymbol() {
+    this.classList.add("open", "show");
+    openCard=this;
+    openCardList(openCard);
+    //console.log(e.classList + " --> These are updated classes.");
+}
+
+function openCardList(e) {
+    console.log(e.innerHTML);
+    len = turnedCards.length;
+    //var child = this.childNodes[0];
+    //console.log(child + " --> This is the child.");
+    console.log(e + "is now called card1");
+    //console.log("This is the value of" + len + " length of array");
+    len++;
+    //console.log("This is the value of" + len + " length of array after incrementing by 1");
+
+    if (len === 1) {
+            turnedCards.push(e.innerHTML);
+            console.log(turnedCards + " len is 1");
+    } else {
+            turnedCards.push(e.innerHTML);
+            console.log(turnedCards + " len is 2");
+            if (turnedCards[0] === turnedCards[1]) {
+                console.log("it's a match!");
+                lockIntoOpen();                 
+                turnedCards = [];
+            } else {
+                console.log("Try again"); //add function
+                hideSymbol();
+                turnedCards = [];
+            }
+        }
+}
+
+function lockIntoOpen() {
+    totalOpenedCards += 2;
+    if (totalOpenedCards === 16) {
+    displayWinMsg();
+    }
+
+}
+
+function hideSymbol90 {
+
+}
+
+function moveCounter {
+
+}
+
+function displayWinMsg {
+
 }
 /*   - loop through each card and create its HTML
  *   - add each card's HTML to the page
+ * - set up event listener for ea card 
  */
-var cards=startOver(); /*Start Game Over on refresh*/
-const shuffledDecks = shuffle(deck); /*return shuffled deck from original deck arrary*/
+const cards = startOver(); /*Start Game Over on refresh.  */
+const fontsArray = makeDeck(fontMix1); /*make font array from chosen font mix*/
+const shuffledFonts = shuffle(fontsArray); /*shuffle the fonts*/
+console.log("this is the shuffledFonts array that we are using to add event listeners to --> " + shuffledFonts);
+console.log(cards.length + "this is the length of the cards array");
 //?whynotwork?for (const shuffledDeck of shuffledDecks) 
-/* add card images to cards */
-for (i=0; i<cards.length; i++) { 
-	const cardImage=`<i class="fa ${shuffledDecks[i]}"></i>`;
-	console.log(cardImage);
-	console.log(cards);
-	cards[i].innerHTML=cardImage;
-	cards[i].addEventListener('click', openCardList) 
+for (i = 0; i < cards.length; i++) {
+    const cardImage = `<i class="fa fa-${shuffledFonts[i]}"></i>`;
+    cards[i].innerHTML = cardImage;
+    cards[i].addEventListener('click', displaySymbol);
+
 }
 
-function openCardList { 
-	     turnedCards.push(this);
-	     console.log(this + "is what this is referring to");
-	     if (turnedCards.length === 2) {
-			if (turnedCards[0] == turnedCards[1]){
-				console.log(it's a match!)
-			}
-		 }
-         //displaySymbol();
-}
-/*
 
-var clicks = 0; // counter 
-var a = document.getElementById('avalue'); // element
-a.onclick = function(b) { // onclick not onClick
-   console.log(++clicks); // increment it
-}
-
-addToOpenCardList(); //and check for match
-         lockInOpenPosition();
-         incrementCounter(); //and display it
-         allMatchDisplay();
-
-document.querySelectorAll */
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
-
-
-
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
+ ***set up the event listener for a card. If a card is clicked:
+ ****display the card's symbol (put this functionality in another function that you call from this one)
+ 
+ ***  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ ****  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
